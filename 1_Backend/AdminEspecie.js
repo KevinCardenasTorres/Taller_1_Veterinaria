f5d7fe5c6e1507577b4fba34776a6ad465517302
KevinCardenasTorres <126635237+KevinCardenasTorres@users.noreply.github.com>
2023-05-21 21:23:48 -0500

    Add files via upload

---
+++ 1_Backend/AdminEspecie.js
@@ -0,0 +1,32 @@
+
+const { PrismaClient } = require ('@prisma/client');
+
+class AdminEspecie
+{
+
+    constructor(){
+        this.prisma = new PrismaClient()
+    }
+    async crearEspecie(req, res){
+        const datos = req.body;
+
+        const especie = await this.prisma.especie.create(
+            {
+                data:datos
+            }
+        );
+
+        res.json(especie);
+
+    }
+
+    async listarEspecie(req, res){
+
+        const especies = await this.prisma.especie.findMany();
+
+        res.json(especies);
+    }
+
+}
+
+module.exports= AdminEspecie;
